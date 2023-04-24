11d340e812b5750d7ce0920432e1e342491ba361
Simon Kirk <124763876+simonkirk007@users.noreply.github.com>
2023-04-24 08:40:20 +0000

    1

---
+++ assets/app.js
@@ -0,0 +1,93 @@
+$(".openbtn").click(function () {
+  $("#myNav").addClass("active");
+});
+
+$(".closebtn").click(function () {
+  $("#myNav").removeClass("active");
+});
+
+$(".search a").click(function () {
+  $(".search-bar").addClass("active");
+});
+
+$(".search-close-ic a").click(function () {
+  $(".search-bar").removeClass("active");
+});
+
+$(".product-slider").slick({
+  dots: false,
+  infinite: true,
+  autoplay: true,
+  arrows: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  prevArrow:
+    '<div class="slick-prev"><i class="fal fa-chevron-left"></i></div>',
+  nextArrow:
+    '<div class="slick-next"><i class="fal fa-chevron-right"></i></div>',
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
+});
+
+$(".image-slider-main").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: ".content-slider-main",
+});
+$(".content-slider-main").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  asNavFor: ".image-slider-main",
+  prevArrow:
+    '<div class="slick-prev"><i class="fal fa-chevron-left"></i></div>',
+  nextArrow:
+    '<div class="slick-next"><i class="fal fa-chevron-right"></i></div>',
+  dots: false,
+  arrows: true,
+  focusOnSelect: true,
+});
+
+$(".product-main-img-slider").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  asNavFor: ".product-detail-btnslider",
+});
+$(".product-detail-btnslider").slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  arrows: false,
+  asNavFor: ".product-main-img-slider",
+});
+
+// Initialize Wow
+new WOW().init();
